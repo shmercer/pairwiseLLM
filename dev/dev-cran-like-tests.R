beece9169abff276b417c29cea9e5bea08c605fe
Sterett H. Mercer <sterett.mercer@ubc.ca>
2025-12-17 17:41:08 -0800

    add tests w/o API keys

---
+++ dev/dev-cran-like-tests.R
@@ -0,0 +1,16 @@
+Sys.setenv(
+  OPENAI_API_KEY = "",
+  ANTHROPIC_API_KEY = "",
+  GEMINI_API_KEY = "",
+  TOGETHER_API_KEY = "",
+  CI = "true",
+  NOT_CRAN = "false"
+)
+
+options(
+  # Don’t accidentally use an http proxy that gives you internet
+  internet.info = FALSE
+)
+
+# Run checks
+devtools::test()
